219e68aad0ba07bd0aaab7f16e8b4a74d6b25bf4
andreydro <andreydro14@gmail.com>
2018-09-17 18:59:44 +0300

    Finish chapter 3

--- chapter_3/duplicated.rb
+++ chapter_3/duplicated.rb
@@ -4,29 +4,12 @@ class Computer
     @data_source = data_source
   end
 
-  def mouse
-    info = @data_source.get_mouse_info(@id)
-    price = @data_source.get_mouse_price(@id)
-    result = "Mouse: #{info} ($#{price})"
+  def method_missing(name)
+    super if !@data_source.respond_to?("get_#{name}_info")
+    info = @data_source.send("get_#{name}_info", @id)
+    price = @data_source.send("get_#{name}_price", @id)
+    result = "#{name.capitalize}: #{info} ($#{price})"
     return "* #{result}" if price >= 100
     result
   end
-
-  def cpu
-    info = @data_source.get_cpu_info(@id)
-    price = @data_source.get_cpu_price(@id)
-    result = "Cpu: #{info} ($#{price})"
-    return "* #{result}" if price >= 100
-    result
-  end
-
-  def keyboard
-    info = @data_source.get_keyboard_info(@id)
-    price = @data_source.get_keyboard_price(@id)
-    result = "Keyboard: #{info} ($#{price})"
-    return "* #{result}" if price >= 100
-    result
-  end
-
-  # ...
 end
