23e1e5d7e54077c90924d5554fc2a019ceeac11b
andreydro <andreydro14@gmail.com>
2018-09-27 18:15:26 +0300

    Finish chapter 6

---
+++ chapter_6/modules.rb
@@ -0,0 +1,16 @@
+module M1
+	def self.included(othermod)
+		puts "M1 was included into #{othermod}"
+	end
+end
+
+module M2
+	def self.prepended(othermod)
+		puts "M2 was prepended to #{othermod}"
+	end
+end
+
+class C
+	include M1
+	prepend M2
+end
